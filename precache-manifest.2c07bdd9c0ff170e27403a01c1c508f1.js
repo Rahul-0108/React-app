e678a761919aea2f251dc3dd14d0774129e47828
Rahul Jaiswal <Rahul.Jaiswal@bentley.com>
2021-01-25 18:39:18 +0530

    Updates

---
+++ precache-manifest.2c07bdd9c0ff170e27403a01c1c508f1.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f80d0251aafb4e819298c153640cafe",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "86cbdd5f8423cfdfe499",
+    "url": "/React-app/static/js/main.9ef084e1.chunk.js"
+  },
+  {
+    "revision": "dfcd39139c89f21a641d",
+    "url": "/React-app/static/js/runtime-main.77357abe.js"
+  }
+]);
