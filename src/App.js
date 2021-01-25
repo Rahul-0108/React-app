a937b8c6e5e7269a02fdbfab796be36549952622
Rahul-Jaiswal-dev <74811698+Rahul-Jaiswal-dev@users.noreply.github.com>
2021-01-25 19:37:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends React.Component {
 return (
 <div>
 
-<h1 style={{backgroundColor :"black",color : "white" , padding : "8px" , fontSize : "28px" ,  width:"88%"}}>Welcome to BookShare App</h1>
+<h1 style={{backgroundColor :"black",color : "white" , padding : "8px" , fontSize : "28px" ,  width:"88%"}}>Welcome to BookShare Apps</h1>
 
 <div className="container">
 <form onSubmit={this.handleSubmit}>
